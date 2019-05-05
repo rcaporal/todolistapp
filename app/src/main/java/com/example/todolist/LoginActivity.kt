3ca42f23c35155b5d5f60c203d8e13f57c99a5ba
Rafael Caporal <rafaelcaporal@gmail.com>
2019-05-05 16:31:55 -0300

    Integraçao com Facebook + Criaçao das Activities

--- app/src/main/java/com/example/todolist/LoginActivity.kt
+++ app/src/main/java/com/example/todolist/LoginActivity.kt
@@ -3,14 +3,11 @@ package com.example.todolist
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class SplashActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
+        setContentView(R.layout.activity_login)
 
     }
-
-
 }
